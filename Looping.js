ddb32b4b3a3e7d639032e7882d529c8447aebacf
Yosephine Mellyana <42544640+Phinz90@users.noreply.github.com>
2018-09-01 12:07:52 +0700

    Add files via upload

---
+++ Looping.js
@@ -0,0 +1,109 @@
+//LOOPING MENGGUNAKAN WHILE
+//LOOPING PERTAMA
+var orang = 0
+while (orang < 20){
+  orang = orang + 2;
+  console.log (orang + ' - I love coding');
+}
+//LOOPING KEDUA
+var orang = 22
+while (orang > 2){
+  orang = orang - 2;
+  console.log (orang + ' - I will become fullstack developer');
+}
+
+
+//LOOPING MENGGUNAKAN FOR
+//LOOPING PERTAMA
+for ( orang = 0; orang <= 20; orang = orang + 1 ) {
+  console.log (orang + '- I love coding')
+  }
+
+//LOOPING KEDUA
+for ( orang = 20; orang >= 1 ; orang = orang - 1) {
+  console.log (orang + '- I will become fullstack developer')
+}
+
+//ANGKA GANJIL/GENAP
+var angka = 0
+for (var angka = 1; angka <= 100; angka++){
+  if(angka % 2 == 1 ){
+    console.log (angka + ' GANJIL');
+  }
+  else if (angka % 2 ===0 ) {
+    console.log (angka + ' GENAP');
+  }
+}
+
+// PERULANGAN- KELIPATAN
+var angka = 0
+for ( var angka = 1; angka <= 100; angka += 2) {
+  if(angka % 3 == 0){
+    console.log ( angka + ' Kelipatan 3')
+  }else 
+  console.log ('')
+}
+
+var angka = 0
+for ( var angka = 1; angka <= 100; angka += 5) {
+  if(angka % 3 == 0){
+    console.log ( angka + ' Kelipatan 3')
+  }else 
+  console.log ('')
+}
+
+var angka = 0
+for ( var angka = 1; angka <= 100; angka += 9) {
+  if(angka % 3 == 0){
+    console.log ( angka + ' Kelipatan 3')
+  }else 
+  console.log ('')
+}
+
+var angka = 0
+for ( var angka = 1; angka <= 100; angka += 2) {
+  if(angka % 3 == 1){
+    console.log ( angka + ' Kelipatan 3')
+  }else 
+  console.log ('')
+}
+
+var angka = 0
+for ( var angka = 1; angka <= 100; angka += 5) {
+  if(angka % 6 == 0){
+    console.log ( angka + ' Kelipatan 6')
+  }else 
+  console.log ('')
+}
+
+var angka = 0
+for ( var angka = 1; angka <= 100; angka += 9) {
+  if(angka % 10 == 0){
+    console.log ( angka + ' Kelipatan 10')
+  }else 
+  console.log ('')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 1. INISIALISASI
+// 2. LIMIT
+// 3. INCREMENT/DECREMENT
+
+                                       
