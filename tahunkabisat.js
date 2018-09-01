d0ba6d9938e61752f05f96332210490f8f232597
Yosephine Mellyana <42544640+Phinz90@users.noreply.github.com>
2018-09-01 14:18:05 +0700

    Add files via upload

---
+++ tahunkabisat.js
@@ -0,0 +1,52 @@
+//STORE 'm' with 600
+//STORE 'a' with 2
+//STORE '∑F' without value
+//CALCULATE 'm' times 'a'
+//SET '∑F' with calculation result
+
+//Masukkan Masa benda adalah 600
+//Masukkan percepatan benda adalah 2
+//ditanyakan resultan gaya
+//resultan gaya adalah masa benda x percepatan benda
+//resultan gaya adalah 1200
+
+
+//STORE TAHUN WITH ANY VALUE
+//STORE TANGGAL WITH 29
+//IF TAHUN % 400 == 1
+//DISPLAY 'TAHUN KABISAT'
+//IF TAHUN % 400 !== 1
+//ELSE IF TAHUN % 100 !== 1
+//ELSE IF TAHUN % 4 !== 1
+//DISPLAY 'BUKAN TAHUN KABISAT'
+
+
+//Diketahui Tanggal 29 adalah hari kabisat
+//jika angka tahun itu habis dibagi 400, maka tahun itu adalah tahun kabisat
+//jika angka tahun itu tidak habis dibagi 400, tapi habis dibagi 4, maka tahun itu adalah tahun kabisat
+//jika angka tahun tidak habis dibagi 400, tidak habis dibagi 100, dan tidak habis dibagi 4, maka tahun itu bukan tahun kabisat
+
+
+//Diketahui Foxie akan mencuci baju menggunakan mesin cuci
+//Mesin cuci akan menyala jika baju mencapai 20
+//Baju Foxie sudah 20
+
+//STORE 'clothes' with ANY VALUE
+//WHILE 'clothes' < 20
+//DISPLAY 'washing machine is OFF'
+//ADD clothes with '20'
+//DISPLAY 'washing machine is ON'
+//DISPLAY 'END PROGRAM'
+
+
+//Diketahui guru akan memeriksa kuku siswa-siswi dikelas sebanyak 40 orang
+//Jika guru menemukan siswa-siswi yg berkuku panjang, maka guru akan menghukum siswa-siswi tersebut
+
+//STORE STUDENTS WITH ANY value
+//STORE COUNTER WITH 0
+//WHILE COUNTER  < STUDENTS
+//IF KUKU PANJANG THEN
+//DISPLAY HUKUM siswa
+//ELSE IF KUKU PENDEK
+//DISPLAY APPRECIATE STUDENTS
+//DISPLAY END PROGRAM
